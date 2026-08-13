03575df61509e71f17c1d7f208dc1afc5d447ea3
airdrophubgroup <163522307+airdrophubgroup@users.noreply.github.com>
2026-08-13 09:46:50 +0530

    Add on-chain payment settlement API

---
+++ api/settle-match.js
@@ -0,0 +1,6 @@
+your-project/
+  api/
+    settle-match.js   <-- yahan
+  index.html
+  style.css
+  app.js
--- dice-duel-next/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OiLNlVFvnbrHN0/rJhptZbdP3QosgF1h5DrwyU30Xng=\"\n}"
